5931cdb2bb6f9bca55f0aa7359d5261da6c26891
Emanuel <emanuel0xb@gmail.com>
2023-09-01 15:06:52 +0200

    Include ar3_base launch by default and shutdown on RViz exit

--- launch/moveit.launch.py
+++ launch/moveit.launch.py
@@ -1,27 +1,38 @@
 import os
-import yaml
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, Command
-from launch.conditions import IfCondition, UnlessCondition
-from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
-from ament_index_python.packages import get_package_share_directory
+
 import xacro
+import yaml
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, EmitEvent
+from launch.conditions import IfCondition
+from launch.events import Shutdown
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
 
 def load_yaml(package_name, *paths):
     package_path = get_package_share_directory(package_name)
     absolute_file_path = os.path.join(package_path, *paths)
 
     try:
-        with open(absolute_file_path, 'r') as file:
+        with open(absolute_file_path, "r") as file:
             return yaml.safe_load(file)
-    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
         return None
 
 
 def generate_launch_description():
     declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="false",
+            description="Start robot with fake hardware mirroring command to its states.",
+        )
+    )
 
     declared_arguments.append(
         DeclareLaunchArgument(
@@ -31,7 +42,9 @@ def generate_launch_description():
         )
     )
 
-    rviz_config_file_default = os.path.join(get_package_share_directory('ar3_moveit2_config'), 'rviz', 'moveit.rviz')
+    rviz_config_file_default = os.path.join(
+        get_package_share_directory("ar3_moveit2_config"), "rviz", "moveit.rviz"
+    )
     declared_arguments.append(
         DeclareLaunchArgument(
             "rviz_config_file",
@@ -40,24 +53,45 @@ def generate_launch_description():
         )
     )
 
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
     start_rviz_0 = LaunchConfiguration("start_rviz_0")
     rviz_config_file = LaunchConfiguration("rviz_config_file")
 
+    declared_arguments.append(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [
+                    os.path.join(get_package_share_directory("ar3_bringup"), "launch"),
+                    "/ar3_base.launch.py",
+                ]
+            ),
+            launch_arguments={
+                use_fake_hardware: use_fake_hardware,
+                "start_rviz": "false",
+            }.items(),
+        )
+    )
+
     # planning_context
     robot_description_config = xacro.process_file(
-        os.path.join(get_package_share_directory("ar3_description"),
-                     "urdf", "ar3.urdf.xacro"))
+        os.path.join(
+            get_package_share_directory("ar3_description"), "urdf", "ar3.urdf.xacro"
+        )
+    )
     robot_description = {"robot_description": robot_description_config.toxml()}
 
     # Find and parse SRDF xacro
-    srdf_xacro_path = os.path.join(get_package_share_directory('ar3_moveit2_config'),
-                                   'config', 'ar3_arm.srdf.xacro')
+    srdf_xacro_path = os.path.join(
+        get_package_share_directory("ar3_moveit2_config"),
+        "config",
+        "ar3_arm.srdf.xacro",
+    )
 
-    robot_description_semantic = {'robot_description_semantic' :
-                                  Command(['xacro', ' ', srdf_xacro_path])}
+    robot_description_semantic = {
+        "robot_description_semantic": Command(["xacro", " ", srdf_xacro_path])
+    }
 
-    kinematics_yaml = load_yaml(
-        'ar3_moveit2_config', 'config', 'kinematics.yaml')
+    kinematics_yaml = load_yaml("ar3_moveit2_config", "config", "kinematics.yaml")
     robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
 
     # Planning Functionality
@@ -68,11 +102,13 @@ def generate_launch_description():
             "start_state_max_bounds_error": 0.1,
         }
     }
-    ompl_planning_yaml = load_yaml('ar3_moveit2_config', 'config', 'ompl_planning.yaml')
-    ompl_planning_pipeline_config['move_group'].update(ompl_planning_yaml)
+    ompl_planning_yaml = load_yaml("ar3_moveit2_config", "config", "ompl_planning.yaml")
+    ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
 
     # Trajectory Execution Functionality
-    moveit_simple_controllers_yaml = load_yaml('ar3_moveit2_config', 'config', 'ar3_controllers.yaml')
+    moveit_simple_controllers_yaml = load_yaml(
+        "ar3_moveit2_config", "config", "ar3_controllers.yaml"
+    )
     moveit_controllers = {
         "moveit_simple_controller_manager": moveit_simple_controllers_yaml,
         "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
@@ -96,7 +132,7 @@ def generate_launch_description():
     run_move_group_node = Node(
         package="moveit_ros_move_group",
         executable="move_group",
-        output="screen",
+        output="log",
         parameters=[
             robot_description,
             robot_description_semantic,
@@ -121,6 +157,7 @@ def generate_launch_description():
             kinematics_yaml,
         ],
         condition=IfCondition(start_rviz_0),
+        on_exit=EmitEvent(event=Shutdown()),
     )
 
     # Static TF
@@ -132,9 +169,6 @@ def generate_launch_description():
         arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "ar3_link0"],
     )
 
-    nodes = [
-        rviz_node,
-        run_move_group_node
-    ]
+    nodes = [rviz_node, run_move_group_node]
 
     return LaunchDescription(declared_arguments + nodes)
